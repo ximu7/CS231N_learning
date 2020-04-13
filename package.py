e5a99fd4e906718de6797a5b94a245b3a25a310b
ximu7 <3130103051@zju.edu.cn>
2020-04-13 21:16:57 +0800

    台式机

--- package.py
+++ package.py
@@ -1,10 +1,9 @@
 
-
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
-x = torch.FloatTensor([0.1,1,10,100,1000,10000]).view(1,-1,1,1)
+x = torch.FloatTensor([0.1, 1, 10, 100, 1000, 10000]).view(1, -1, 1, 1)
 x = Variable(x)
 print(x.data.size())
 conv = nn.Conv2d(in_channels=6,
@@ -17,9 +16,9 @@ conv = nn.Conv2d(in_channels=6,
 
 print(conv.weight.data.size())
 # [1,2,3,4,5,6,7,8,9,10,11,12]
-conv.weight.data =torch.arange(1,13).view(6,2,1,1).float()
+conv.weight.data = torch.arange(1, 13).view(6, 2, 1, 1).float()
 
 # print(conv.weight.data)
 
-output=conv(x)
+output = conv(x)
 print(output.data.size())
